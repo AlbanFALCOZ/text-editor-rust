50e8fcfedd3a4aff09ee466c4de7fb86c7fd2030
Alban FALCOZ <alban.falcoz@orange.fr>
2024-08-19 14:58:52 +0200

    feat: print tildes at beginning of rows

--- src/editor.rs
+++ src/editor.rs
@@ -2,65 +2,107 @@
 
 mod terminal;
 
-use crate::editor::terminal::Terminal;
-use crossterm::event::{read, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
+use crate::editor::terminal::{Position, Size, Terminal};
+use crossterm::event::{read, Event, Event::Key, KeyCode, KeyCode::Char, KeyModifiers};
+use crossterm::style::{Color, Colors};
 use std;
 use std::io::Error;
 
 pub struct Editor {
     should_quit: bool,
+    pos: Position,
 }
 
 impl Editor {
     pub fn default() -> Self {
-        Editor { should_quit: false }
+        Editor {
+            should_quit: false,
+            pos: Position::default(),
+        }
     }
 
     pub fn set_up() -> Result<(), Error> {
         Terminal::enable_raw_mode()?;
         Terminal::clear_screen()?;
-        Terminal::move_cursor_to(0, 0)?;
+
+        //TODO
+        //not working
+        //Terminal::set_size(Size{length: 180, height: 38})?;
+        //Terminal::execute()?;
+
+        Terminal::set_color(Colors {
+            foreground: Option::from(Color::Green),
+            background: None,
+        })?;
+        Self::print_rows(Terminal::get_size()?.height);
+        Terminal::move_cursor_to(&Position::default())?;
         Terminal::execute()
     }
 
     pub fn quit() -> Result<(), Error> {
         Terminal::disable_raw_mode()?;
         Terminal::clear_screen()?;
-        Terminal::move_cursor_to(0,0)?;
+        Terminal::reset_color()?;
+        Terminal::move_cursor_to(&Position::new(0, 0))?;
         Terminal::execute()?;
         Ok(())
     }
 
     pub fn run(&mut self) {
-        if let Err(err) = self.repl() {
-            println!("{err:#?}");
-        }
-        println!("Goodbye ~~!");
+        Self::set_up().unwrap();
+        let result = self.repl();
+        Self::quit().unwrap();
+        result.unwrap();
+        println!("Goodbye ! ~~");
     }
 
     pub fn repl(&mut self) -> Result<(), Error> {
         Self::set_up()?;
-        loop {
-            if let Key(KeyEvent {
-                code,
-                modifiers,
-                kind,
-                state,
-            }) = read()?
-            {
-                println!("Code {code:?}, keyModifiers : {modifiers:?}, kind : {kind:?}, state : {state:?} \r");
-                match code {
-                    Char('q') if modifiers == KeyModifiers::CONTROL => {
-                        self.should_quit = true;
-                    }
-                    _ => (),
-                }
-            }
-            if self.should_quit {
-                break;
-            }
+        while !self.should_quit {
+            let event = read()?;
+            self.evaluate_event(&event)?;
         }
+
         Self::quit()?;
+        Ok(())
+    }
+
+    pub fn evaluate_event(&mut self, event: &Event) -> Result<(), Error> {
+        match event {
+            Key(event) => {
+                if event.modifiers == KeyModifiers::CONTROL && event.code == Char('q') {
+                    self.should_quit = true;
+                }
+                Terminal::move_cursor_to(&self.pos)?;
+                println!("{:?}", event);
+                Ok(())
+            }
+            Event::Resize(.., height) => {
+                Self::print_rows(*height);
+                let mut size: Size = Terminal::get_size()?;
+                size.length = size.length - 30;
+                Self::print_version(Position {
+                    x: size.length,
+                    y: size.height,
+                })?;
+                Ok(())
+            }
+            _ => Ok(()),
+        }
+    }
+
+    pub fn print_rows(height: u16) {
+        for _empty_line in 0..height / 3 {
+            println!();
+        }
+        for _row in height / 3..height {
+            println!("~");
+        }
+    }
+
+    pub fn print_version(position: Position) -> Result<(), Error> {
+        Terminal::move_cursor_to(&position)?;
+        println!("Rust terminal ver 0.5");
         Ok(())
     }
 }
--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,12 +1,31 @@
 #![warn(clippy::all, clippy::pedantic, clippy::print_stdout)]
 
-use std::io::{Error, stdout, Write};
 use crossterm;
-use crossterm::{queue, Command};
+use crossterm::style::{Color, Colors};
 use crossterm::terminal::ClearType;
+use crossterm::{queue, Command};
+use std::io::{stdout, Error, Write};
+
+#[derive(Copy, Clone, Default)]
+pub struct Position {
+    pub x: u16,
+    pub y: u16,
+}
+
+
+impl Position {
+    pub fn new(x: u16, y: u16) -> Position {
+        Position { x, y }
+    }
+}
+
+#[derive(Copy, Clone, Default)]
+pub struct Size {
+    pub length: u16,
+    pub height: u16,
+}
 
 pub struct Terminal {}
-
 
 impl Terminal {
     pub fn disable_raw_mode() -> Result<(), Error> {
@@ -19,27 +38,47 @@ impl Terminal {
         Ok(())
     }
 
+    pub fn set_size(size: Size) -> Result<(), Error> {
+        Self::execute_command(crossterm::terminal::SetSize(size.length, size.height))?;
+        Ok(())
+    }
+
+    pub fn get_size() -> Result<Size, Error> {
+        let result = crossterm::terminal::size()?;
+        let size: Size = Size {
+            length: result.0,
+            height: result.1,
+        };
+        Ok(size)
+    }
+
     pub fn clear_screen() -> Result<(), Error> {
         Self::execute_command(crossterm::terminal::Clear(ClearType::All))?;
         Ok(())
     }
 
-    pub fn move_cursor_to(x: u16, y: u16) -> Result<(), Error> {
-        Self::execute_command(crossterm::cursor::MoveTo(x,y))?;
+    pub fn move_cursor_to(position: &Position) -> Result<(), Error> {
+        Self::execute_command(crossterm::cursor::MoveTo(position.x, position.y))?;
+        Ok(())
+    }
+
+    pub fn set_color(colors: Colors) -> Result<(), Error> {
+        Self::execute_command(crossterm::style::SetColors(colors))?;
+        Ok(())
+    }
+
+    pub fn reset_color() -> Result<(), Error> {
+        Self::execute_command(crossterm::style::ResetColor)?;
+        Ok(())
+    }
+
+    fn execute_command<T: Command>(command: T) -> Result<(), Error> {
+        queue!(stdout(), command)?;
         Ok(())
     }
 
     pub fn execute() -> Result<(), Error> {
         stdout().flush()?;
-        Ok(())
-    }
-
-    /*pub fn write() -> Result<(), Error> {
-        Self::execute_command(write!())
-    }*/
-
-    fn execute_command<T: Command>(command: T) -> Result<(), Error> {
-        queue!(stdout(), command)?;
         Ok(())
     }
 }
