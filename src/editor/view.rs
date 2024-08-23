05d03050277ef836d3d8322daa55e773f2c83a38
Alban FALCOZ <alban.falcoz@orange.fr>
2024-08-23 15:54:59 +0200

    feat: Split the render method into two different method : render_welcome_message and render_buffer

--- src/editor/view.rs
+++ src/editor/view.rs
@@ -1,41 +1,61 @@
-use std::io::Error;
 use crate::editor::terminal::{Size, Terminal};
 use crate::editor::view::buffer::Buffer;
+use std::io::Error;
 
 mod buffer;
 
 #[derive(Default)]
 pub struct View {
-    buffer: Buffer
+    buffer: Buffer,
 }
 
-
 impl View {
-
     /// Print the lines.
     /// It prints tilde '~' at the beginning of each line.
     /// Print the terminal version at 1/3 of the screen.
     /// The clippy warning is disabled because it doesn't matter if the version is exactly at 1/3 of our screen.
     pub fn render(&self) -> Result<(), Error> {
+        if self.buffer.is_empty() {
+            self.render_welcome_message()?;
+        }
+        else {
+            self.render_buffer()?;
+        }
+        Ok(())
+    }
+    
+    fn render_buffer(&self) -> Result<(), Error> {
         let Size { height, .. } = Terminal::get_size()?;
-        Terminal::clear_line()?;
         for current_row in 0..height {
             Terminal::clear_line()?;
-            if let Some(line) = self.buffer.lines.get(current_row){
+            if let Some(line) = self.buffer.lines.get(current_row) {
                 Terminal::print(line)?;
-                Terminal::print("\r\n")?;
-                continue;
             }
-            #[allow(clippy::integer_division)]
-            if current_row == height / 3 && self.buffer.is_empty(){
-                Self::draw_welcome_message()?;
-            } else {
+            else {
                 Self::draw_empty_row()?;
             }
             if current_row.saturating_add(1) < height {
                 Terminal::print("\r\n")?;
             }
         }
+        Ok(())
+    }
+
+    fn render_welcome_message(&self) -> Result<(), Error> {
+        let Size { height, .. } = Terminal::get_size()?;
+        for current_row in 0..height {
+            Terminal::clear_line()?;
+            #[allow(clippy::integer_division)]
+            if current_row == height/3 {
+                Self::draw_welcome_message()?;
+            }
+            else {
+                Self::draw_empty_row()?;
+            }
+            if current_row.saturating_add(1) < height {
+                Terminal::print("\r\n")?;
+            }
+        };
         Ok(())
     }
 
